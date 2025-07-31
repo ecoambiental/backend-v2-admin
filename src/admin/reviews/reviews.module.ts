5b6fbb0976c0f07f4081d574d18d53a429cea950
Renzo <renzofab2@hotmail.com>
2025-07-30 19:33:02 -0500

    agregando guards

--- src/admin/reviews/reviews.module.ts
+++ src/admin/reviews/reviews.module.ts
@@ -3,10 +3,11 @@ import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course, Enrollment } from 'ingepro-entities';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ReviewsController],
   providers: [ReviewsService],
-  imports: [TypeOrmModule.forFeature([Course, Enrollment])],
+  imports: [TypeOrmModule.forFeature([Course, Enrollment]), AuthModule],
 })
 export class ReviewsModule {}
